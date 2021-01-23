2f5d0b6bfcb07b16b37bee446e6bda1245e46897
Eduardo Santos <76977508+dudukb@users.noreply.github.com>
2021-01-23 12:25:54 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiaperguntas','root','123456', {
+const connection = new Sequelize('guiaperguntas','root','DUDU_1234', {
     host: 'localhost',
     dialect: 'mysql'
 });
